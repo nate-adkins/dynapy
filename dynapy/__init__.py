8428b289ca6a8e03994e06bc9f482415a96789ea
Nate <113215344+nate-adkins@users.noreply.github.com>
2025-02-11 23:45:30 -0500

    dynamixel python library

---
+++ dynapy/__init__.py
@@ -0,0 +1,3 @@
+from dynapy.dynamixel import Dynamixel, U2D2
+from dynapy.dynamixel_models import MX106, XL320, XL430, XL430W250, XM540W150, MX28, MX64, XM430W210
+from dynapy.ctrl_table_value import _CtrlTableValue
---
+++ install_dynamixel_sdk.bash
@@ -0,0 +1,7 @@
+git clone git@github.com:ROBOTIS-GIT/DynamixelSDK.git dynamixel_sdk
+cd dynamixel_sdk/python
+pip install .
+# sudo python3 setup.py install
+cd ../..
+pip install .
+# sudo python3 setup.py install
